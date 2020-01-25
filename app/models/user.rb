845c7bbdb743d60fc480a0e23161e9f35a581ce5
Keemo Watson <keemowatson1@gmail.com>
2020-01-25 21:16:08 +0000

    fixed seeds so the each sport show actual sport location

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
 
     has_secure_password
     validates :email, presence: true, uniqueness: true
-
     
 end
--- db/migrate/20200122123419_create_day_types.rb
+++ db/migrate/20200122123419_create_day_types.rb
@@ -2,7 +2,7 @@ class CreateDayTypes < ActiveRecord::Migration[6.0]
   def change
     create_table :day_types do |t|
       t.string :name
-      t.string :image_link
+      t.string :image_link 
 
       t.timestamps
     end
